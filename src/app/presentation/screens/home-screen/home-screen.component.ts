6010006362e288242a61395581dbf3fb5895fd6e
Ahishek kushwah <singhabhishek72764@gmail.com>
2024-02-15 23:36:24 +0530

    footer template

--- src/app/presentation/screens/home-screen/home-screen.component.ts
+++ src/app/presentation/screens/home-screen/home-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-screen',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './home-screen.component.html',
   styleUrl: './home-screen.component.scss'
 })
