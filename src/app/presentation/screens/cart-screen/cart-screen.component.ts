92ac14310b04bdb8b3198c525da7c19ad95fe9ef
Ahishek kushwah <singhabhishek72764@gmail.com>
2024-02-26 22:18:08 +0530

    shopping cart

--- src/app/presentation/screens/cart-screen/cart-screen.component.ts
+++ src/app/presentation/screens/cart-screen/cart-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
+import { IncrDecrButtonComponent } from '../../components/incr-decr-button/incr-decr-button.component';
 
 @Component({
   selector: 'app-cart-screen',
   standalone: true,
-  imports: [BreadcrumbComponent],
+  imports: [BreadcrumbComponent,IncrDecrButtonComponent],
   templateUrl: './cart-screen.component.html',
   styleUrl: './cart-screen.component.scss'
 })
